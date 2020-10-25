58c1ee1f7bfd30190f4587aa84fab559a2a3f764
vera-nekrasova <vera.nekrasova.lyfrie@gmail.com>
2020-10-25 23:57:25 +0700

    delete folder src

--- src/script/smoothNavigation.js
+++
@@ -1,9 +0,0 @@
-function smoothNavigation(blockShow) {
-	let blockID = blockShow.getAttribute('href');
-
-	document.querySelector(blockID).scrollIntoView({
-		behavior: 'smooth',
-	})
-}
-
-export { smoothNavigation }
